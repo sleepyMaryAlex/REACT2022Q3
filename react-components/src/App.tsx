c51b3c8e274aae557585a30ea8149a586f4f7f04
Maria Guchkova <mariaguchkova1@gmail.com>
2022-09-28 17:17:47 +0300

    feat: add router

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,5 +1,9 @@
+import AboutPage from 'components/AboutPage/AboutPage';
 import Header from 'components/Header/Header';
+import Main from 'components/Main/Main';
+import NotFound from 'components/NotFound/NotFound';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 class App extends React.Component {
@@ -7,6 +11,11 @@ class App extends React.Component {
     return (
       <div className="app">
         <Header />
+        <Routes>
+          <Route index element={<Main />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     );
   }
