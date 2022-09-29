7f07dc013cf130003bce8dc2d7cb5b22f6762403
Maria Guchkova <mariaguchkova1@gmail.com>
2022-09-29 18:02:53 +0300

    feat: add results of search

---
+++ react-components/src/components/Results/Results.tsx
@@ -0,0 +1,27 @@
+import Cards from 'components/Cards/Cards';
+import Dropdown from 'components/Dropdown/Dropdown';
+import React from 'react';
+import { IResults } from 'types/types';
+import './Results.css';
+
+class Results extends React.Component<IResults> {
+  constructor(props: IResults) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="results">
+        <div className="results__container">
+          <Dropdown
+            cards={this.props.cards}
+            handleClickByCuisine={this.props.handleClickByCuisine}
+            handleClickByDiet={this.props.handleClickByDiet}
+          />
+        </div>
+        <Cards cards={this.props.cards} numShow={this.props.numShow} />
+      </div>
+    );
+  }
+}
+
+export default Results;
