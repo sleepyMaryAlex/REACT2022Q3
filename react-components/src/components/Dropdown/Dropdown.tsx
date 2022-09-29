d49eb94b111e57993360211ff9770586d629367d
Maria Guchkova <mariaguchkova1@gmail.com>
2022-09-29 18:03:22 +0300

    feat: add dropdown

---
+++ react-components/src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { IDropdown } from 'types/types';
+import './Dropdown.css';
+
+class Dropdown extends React.Component<IDropdown, { cuisine: string[]; diet: string[] }> {
+  constructor(props: IDropdown) {
+    super(props);
+    this.state = {
+      cuisine: [
+        'AFRICAN',
+        'AMERICAN',
+        'BRITISH',
+        'CHINESE',
+        'GERMAN',
+        'ITALIAN',
+        'JAPANESE',
+        'NORDIC',
+      ],
+      diet: [
+        'GLUTEN FREE',
+        'KETOGENIC',
+        'VEGETARIAN',
+        'PRIMAL',
+        'VEGAN',
+        'PALEO',
+        'LOW FODMAP',
+        'WHOLE30',
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="cards__results">
+        <p className="results__title">{this.props.cards.length} RESULTS</p>
+        <select
+          className="select__coisine"
+          onChange={(event) => this.props.handleClickByCuisine(event)}
+        >
+          <option value="cuisine">ALL CUISINES</option>
+          {this.state.cuisine.map((value) => (
+            <option value={value.toLowerCase()} key={value}>
+              {value}
+            </option>
+          ))}
+        </select>
+        <select className="select__diet" onChange={(event) => this.props.handleClickByDiet(event)}>
+          <option value="diet">ALL DIETS</option>
+          {this.state.diet.map((value) => (
+            <option value={value.toLowerCase()} key={value}>
+              {value}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  }
+}
+
+export default Dropdown;
