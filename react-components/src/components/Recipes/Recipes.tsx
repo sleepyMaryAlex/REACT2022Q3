81e424168d124b26c21766f1968d07e39b531518
Maria Guchkova <mariaguchkova1@gmail.com>
2022-10-07 09:16:21 +0300

    feat: add recipes component

--- react-components/src/components/Recipes/Recipes.tsx
+++ react-components/src/components/Recipes/Recipes.tsx
@@ -1,9 +1,31 @@
+import RecipeCards from 'components/RecipeCards/RecipeCards';
 import React from 'react';
 import './Recipes.css';
+import { IRecipeCard } from 'types/types';
+import Form from 'components/Form/Form';
 
-class Recipes extends React.Component {
+class Recipes extends React.Component<object, { cards: IRecipeCard[] }> {
+  constructor(props: object) {
+    super(props);
+    this.addNewRecipe = this.addNewRecipe.bind(this);
+    this.state = {
+      cards: [],
+    };
+  }
+
+  addNewRecipe(card: IRecipeCard) {
+    this.setState({
+      cards: [...this.state.cards, card],
+    });
+  }
+
   render() {
-    return <div className="recipes"></div>;
+    return (
+      <div className="recipes">
+        <Form addNewRecipe={this.addNewRecipe} />
+        <RecipeCards cards={this.state.cards} />
+      </div>
+    );
   }
 }
 
