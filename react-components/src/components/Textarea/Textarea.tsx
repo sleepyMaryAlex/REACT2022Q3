70a11018feecfbebfb2606e4302385c7db4f1a2e
Maria Guchkova <mariaguchkova1@gmail.com>
2022-10-07 15:13:42 +0300

    feat: add textarea component

---
+++ react-components/src/components/Textarea/Textarea.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Textarea.css';
+
+class Textarea extends React.Component<{
+  handleDescriptionChange: (description: string) => void;
+  showDescriptionMessage: boolean;
+}> {
+  textareaRef: React.RefObject<HTMLTextAreaElement>;
+  constructor(props: {
+    handleDescriptionChange: (description: string) => void;
+    showDescriptionMessage: boolean;
+  }) {
+    super(props);
+    this.textareaRef = React.createRef();
+  }
+
+  handleChange() {
+    const description = this.textareaRef.current?.value as string;
+    this.props.handleDescriptionChange(description);
+  }
+
+  render() {
+    return (
+      <div className="form__textarea-field">
+        <textarea
+          placeholder="HOW TO MAKE?"
+          className="form__textarea"
+          ref={this.textareaRef}
+          spellCheck={false}
+          onChange={this.handleChange.bind(this)}
+        />
+        <p className="form__message textarea__message">
+          {this.props.showDescriptionMessage ? 'This field is required' : ''}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Textarea;
