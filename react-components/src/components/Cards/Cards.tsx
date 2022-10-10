6f90d3a39ddbb4bbe683fcb6e743dcaba6b8a304
Maria Guchkova <mariaguchkova1@gmail.com>
2022-10-10 22:26:33 +0300

    refactor: remove constructor from cards component

--- react-components/src/components/Cards/Cards.tsx
+++ react-components/src/components/Cards/Cards.tsx
@@ -4,10 +4,6 @@ import { ICard, ICards } from 'types/types';
 import './Cards.css';
 
 class Cards extends React.Component<ICards> {
-  constructor(props: ICards) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="cards">
