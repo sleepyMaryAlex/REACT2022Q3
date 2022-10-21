5ebcc69dcf4e01ec46566efdc5c69dd3f88efbe3
Maria Guchkova <mariaguchkova1@gmail.com>
2022-10-21 16:14:25 +0300

    feat: transform Progress component from class to function

--- react-hooks/src/components/Progress/Progress.tsx
+++ react-hooks/src/components/Progress/Progress.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import './Progress.css';
 
-class Progress extends React.Component {
-  render() {
-    return (
-      <div className="circles">
-        <div className="circles-wrapper">
-          <div className="circle circle-lg">
-            <div className="circle circle-md">
-              <div className="circle circle-sm"></div>
-            </div>
+function Progress() {
+  return (
+    <div className="circles">
+      <div className="circles-wrapper">
+        <div className="circle circle-lg">
+          <div className="circle circle-md">
+            <div className="circle circle-sm"></div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Progress;
