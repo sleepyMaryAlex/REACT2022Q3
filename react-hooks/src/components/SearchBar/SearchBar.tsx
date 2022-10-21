2a60144740ee9f8a0ee16e85dfb02721d66cf4a7
Maria Guchkova <mariaguchkova1@gmail.com>
2022-10-21 16:22:07 +0300

    feat: transform SearchBar component from class to function

--- react-hooks/src/components/SearchBar/SearchBar.tsx
+++ react-hooks/src/components/SearchBar/SearchBar.tsx
@@ -3,30 +3,29 @@ import './SearchBar.css';
 import icon from '../../assets/icon.svg';
 import { ISearchBar } from 'types/types';
 
-class SearchBar extends React.Component<ISearchBar> {
-  render() {
-    return (
-      <div className="search">
-        <div className="search__container">
-          <p className="search__title">I want to find</p>
-          <img className="search__image" src={icon} alt="icon" onClick={this.props.handleSubmit} />
-          <input
-            className="search__bar"
-            type="text"
-            value={this.props.query}
-            autoFocus
-            spellCheck="false"
-            onChange={this.props.handleChange}
-            onKeyDown={(event) => {
-              if (event.key === 'Enter') {
-                this.props.handleSubmit();
-              }
-            }}
-          />
-        </div>
+function SearchBar(props: ISearchBar) {
+  const { handleChange, handleSubmit, query } = props;
+  return (
+    <div className="search">
+      <div className="search__container">
+        <p className="search__title">I want to find</p>
+        <img className="search__image" src={icon} alt="icon" onClick={handleSubmit} />
+        <input
+          className="search__bar"
+          type="text"
+          value={query}
+          autoFocus
+          spellCheck="false"
+          onChange={handleChange}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              handleSubmit();
+            }
+          }}
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default SearchBar;
