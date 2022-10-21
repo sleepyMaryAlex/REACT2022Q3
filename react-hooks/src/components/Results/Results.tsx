0446f7ef2f853daca5da1c9a5a188976aff55bab
Maria Guchkova <mariaguchkova1@gmail.com>
2022-10-21 15:36:35 +0300

    feat: transform Results component from class to function

--- react-hooks/src/components/Results/Results.tsx
+++ react-hooks/src/components/Results/Results.tsx
@@ -3,23 +3,21 @@ import React from 'react';
 import { IResults } from 'types/types';
 import './Results.css';
 
-class Results extends React.Component<IResults> {
-  render() {
-    const { count, pages, currentPage, results, setOpenModal, setIndex } = this.props;
-    return (
-      <div className="results">
-        <div className="results__container">
-          <div className="cards__results">
-            <p className="results__title">{count} results</p>
-            <p className="results__title">
-              page {currentPage}/{pages}
-            </p>
-          </div>
+function Results(props: IResults) {
+  const { count, pages, currentPage, results, setOpenModal, setIndex } = props;
+  return (
+    <div className="results">
+      <div className="results__container">
+        <div className="cards__results">
+          <p className="results__title">{count} results</p>
+          <p className="results__title">
+            page {currentPage}/{pages}
+          </p>
         </div>
-        <Cards results={results} setOpenModal={setOpenModal} setIndex={setIndex} />
       </div>
-    );
-  }
+      <Cards results={results} setOpenModal={setOpenModal} setIndex={setIndex} />
+    </div>
+  );
 }
 
 export default Results;
