65313a3ce0d3f72c484aec309ae02e811e896ea1
Maria Guchkova <mariaguchkova1@gmail.com>
2022-10-21 16:17:52 +0300

    feat: transform NotFound component from class to function

--- react-hooks/src/components/NotFound/NotFound.tsx
+++ react-hooks/src/components/NotFound/NotFound.tsx
@@ -3,43 +3,36 @@ import { NavLink } from 'react-router-dom';
 import './NotFound.css';
 import returnImg from '../../assets/prev-arrow.svg';
 
-class NotFound extends React.Component<object> {
-  classNames: string[];
-  error: number;
-  constructor(props: object) {
-    super(props);
-    this.classNames = [
-      'error__last',
-      'error__second',
-      'error__first',
-      'error__basic',
-      'error__first',
-      'error__second',
-      'error__last',
-    ];
-    this.error = 401;
-  }
+function NotFound() {
+  const classNames: string[] = [
+    'error__last',
+    'error__second',
+    'error__first',
+    'error__basic',
+    'error__first',
+    'error__second',
+    'error__last',
+  ];
+  const error = 401;
 
-  render() {
-    return (
-      <div className="not-found__wrapper">
-        <div className="not-found__error">
-          {this.classNames.map((className, index) => (
-            <p className={className} key={(this.error + index).toString()}>
-              {(this.error + index).toString()}
-            </p>
-          ))}
-        </div>
-        <p className="not-found__title">The page you were looking for could not be found</p>
-        <NavLink to="/">
-          <div className="not-found__return-button">
-            <img src={returnImg} alt="image" />
-            <p>Return to main page</p>
-          </div>
-        </NavLink>
+  return (
+    <div className="not-found__wrapper">
+      <div className="not-found__error">
+        {classNames.map((className, index) => (
+          <p className={className} key={(error + index).toString()}>
+            {(error + index).toString()}
+          </p>
+        ))}
       </div>
-    );
-  }
+      <p className="not-found__title">The page you were looking for could not be found</p>
+      <NavLink to="/">
+        <div className="not-found__return-button">
+          <img src={returnImg} alt="image" />
+          <p>Return to main page</p>
+        </div>
+      </NavLink>
+    </div>
+  );
 }
 
 export default NotFound;
