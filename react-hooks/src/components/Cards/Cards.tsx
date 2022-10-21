6ef8c9a36bd2609448bcb35b2d1c89d562891aa4
Maria Guchkova <mariaguchkova1@gmail.com>
2022-10-21 15:38:50 +0300

    feat: transform Cards component from class to function

--- react-hooks/src/components/Cards/Cards.tsx
+++ react-hooks/src/components/Cards/Cards.tsx
@@ -3,23 +3,21 @@ import React from 'react';
 import { ICards, IResult } from 'types/types';
 import './Cards.css';
 
-class Cards extends React.Component<ICards> {
-  render() {
-    const { setOpenModal, setIndex } = this.props;
-    return (
-      <div className="cards">
-        {this.props.results.map((card: IResult, index) => (
-          <Card
-            setOpenModal={setOpenModal}
-            card={card}
-            key={card.id.toString()}
-            setIndex={setIndex}
-            index={index}
-          />
-        ))}
-      </div>
-    );
-  }
+function Cards(props: ICards) {
+  const { setOpenModal, setIndex } = props;
+  return (
+    <div className="cards">
+      {props.results.map((card: IResult, index) => (
+        <Card
+          setOpenModal={setOpenModal}
+          card={card}
+          key={card.id.toString()}
+          setIndex={setIndex}
+          index={index}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Cards;
