9c139c90d6e230fe1a34ef683465c18a8e71c460
Maria Guchkova <mariaguchkova1@gmail.com>
2022-11-03 09:50:00 +0300

    refactor: remove redundant interfaces

--- react-components/src/types/types.ts
+++ react-components/src/types/types.ts
@@ -31,42 +31,9 @@ export interface IData {
   results: IResult[];
 }
 
-export interface IMain {
-  state: IAppState;
-  dispatch: React.Dispatch<IAppAction>;
-}
-
-export interface ISearchBar {
-  dispatch: React.Dispatch<IAppAction>;
-  onSubmit: (query: string) => void;
-}
-
-export interface IResults {
-  state: IAppState;
-  dispatch: React.Dispatch<IAppAction>;
-  handlePageChange: (event: React.ChangeEvent<unknown>, page: number) => void;
-}
-
-export interface ICards {
-  results: IResult[];
-  dispatch: React.Dispatch<IAppAction>;
-}
-
 export interface ICard {
   card: IResult;
-  dispatch: React.Dispatch<IAppAction>;
   index: number;
-}
-
-export interface IAboutCharacter {
-  results: IResult[];
-  index: number | null;
-  dispatch: React.Dispatch<IAppAction>;
-}
-
-export interface IHeader {
-  results: IResult[];
-  index: number | null;
 }
 
 export interface ICharacterCard {
@@ -125,7 +92,7 @@ export interface IMessage {
   setDisplayMessage: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export interface IAppState {
+export interface IMainState {
   results: IResult[];
   count: number;
   currentPage: number;
@@ -148,11 +115,6 @@ export interface IFormState {
   canSubmit: boolean;
   displayErrorMessage: boolean;
   canCheckMistakes: boolean;
-}
-
-export interface IAppAction {
-  type: string;
-  payload?: IResult[] | string | number | boolean | null;
 }
 
 export interface IFormAction {
